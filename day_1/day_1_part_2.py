56e17dcebdbd3b9cc608fcaac81a946b138edcc8
Matthew Chivers <matthewchivers@outlook.com>
2020-12-04 13:52:36 +0000

    Altered formatting for cleaner code
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- day_1/day_1_part_2.py
+++ day_1/day_1_part_2.py
@@ -12,11 +12,9 @@ if __name__ == '__main__':
         nums.append(int(line.strip()))
 
     for x in range(len(nums)):
-        num_x = nums[x]
         for y in range(x, len(nums)):
-            num_y = nums[y]
             for z in range(y, len(nums)):
-                num_z = nums[z]
-                if(num_x + num_y + num_z == 2020):
-                    print(str(num_x) + " + " + str(num_y) + " + " + str(num_z) + " = " + str(num_x + num_y + num_z))
-                    print(str(num_x) + " * " + str(num_y) + " * " + str(num_z) + " = " + str(num_x * num_y * num_z))
+                if(num[x] + num[y] + num[z] == 2020):
+                    print(str(num[x]) + " + " + str(num[y]) + " + " + str(num[z]) + " = " + str(num[x] + num[y] + num[z]))
+                    print(str(num[x]) + " * " + str(num[y]) + " * " + str(num[z]) + " = " + str(num[x] * num[y] * num[z]))
+
